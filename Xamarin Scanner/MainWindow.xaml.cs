bb80626e6d66c27a5ee38015b755c57e1af682fe
Naveen Rajappa <nrajappa94@yahoo.com>
2016-11-03 16:43:47 +0100

    Add files via upload

---
+++ Xamarin Scanner/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace Xamarin_Scanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Generate_QRCode(object sender, RoutedEventArgs e)
+        {
+            var qrcode = new QRCodeWriter();
+            var qrValue = QRText.Text;
+
+            var barcodeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions
+                {
+                    Height = 300,
+                    Width = 300,
+                    Margin = 1
+
+                }
+            };
+
+            using (var bitmap = barcodeWriter.Write(qrValue))
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Png);
+
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                stream.Seek(0, SeekOrigin.Begin);
+                bi.StreamSource = stream;
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.EndInit();
+                QRCode.Source = bi; //A WPF Image control
+            }
+        }
+    }
+}
